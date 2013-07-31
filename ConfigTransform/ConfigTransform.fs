f0bd26ed2f379de4e590540ddc30f66c055ef775
Cameron Taggart <cameron.taggart@gmail.com>
2013-07-31 00:27:22 -0500

    initial setup of ConfigTransform project

---
+++ ConfigTransform/ConfigTransform.fs
@@ -0,0 +1,47 @@
+﻿namespace ConfigTransform
+
+open System
+open System.IO
+open Microsoft.Build.Framework
+open System.Collections.Generic
+
+type ConfigTransform() =
+    inherit Task()
+
+    [<Required>]
+    member val ProjectFile = String.Empty with set, get
+    [<Required>]
+    member val Exclude = String.Empty with set, get
+    [<Required>]
+    member val Configuration = String.Empty with set, get
+    [<Required>]
+    member val EnvVar = String.Empty with set, get
+    
+    member internal x.GetProjectFile() =
+        x.ProjectFile |> Path.GetFullPath
+
+    member internal x.GetConfigFiles() =
+        let excludes = HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        if false = String.IsNullOrEmpty x.Exclude then
+            for exclude in x.Exclude.Split [|';'|] do
+                excludes.Add exclude |> ignore
+        Proj.getConfigFiles (x.GetProjectFile()) excludes
+
+    member internal x.GetEnvVars() =
+        x.EnvVar.Split [|';'|]
+        |> Seq.map (fun var ->
+            var, Environment.GetEnvironmentVariable var
+        )
+
+    override x.Execute() =
+        try
+            for c in x.GetConfigFiles() do
+                x.MessageHigh "config file: %s" c
+            x.MessageHigh "configuration: %s" x.Configuration
+            x.MessageHigh "env vars: %A" (x.GetEnvVars())
+            () // TODO
+        with
+        | _ as ex -> x.Error "%s" ex.Message
+
+        not x.HasErrors
+    
---
+++ ConfigTransform/Proj.fs
@@ -0,0 +1,20 @@
+﻿module ConfigTransform.Proj
+
+open Microsoft.Build.Evaluation
+open System.IO
+open System.Collections.Generic
+
+let pathCombine a b = Path.Combine(a, b)
+
+let getConfigFiles (file:string) (excludes:ISet<string>) =
+    ProjectCollection.GlobalProjectCollection.UnloadAllProjects()
+    let dir = Path.GetDirectoryName file
+    let p = Project(file)
+    p.Items
+    |> Seq.filter (fun i -> i.ItemType = "Content")
+    |> Seq.map (fun i -> i.EvaluatedInclude)
+    |> Seq.filter (fun path -> Path.GetExtension path = ".config")
+    |> Seq.filter (fun path -> false = (Path.GetFileNameWithoutExtension path).Contains ".")
+    |> Seq.filter (fun path -> false = excludes.Contains path)
+    |> Seq.map (fun path -> pathCombine dir path |> Path.GetFullPath)
+    |> Seq.toArray
